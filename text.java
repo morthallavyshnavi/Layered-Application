1f159a7f8e25b6df0a1a1410a02193c0c6d7e04d
vyshu <morthallavyshnavireddy@gmail.com>
2025-01-09 21:57:21 +0530

    initial commit

--- text.java
+++ text.java
@@ -1,4 +1,8 @@
 public class Emp{
+    public void subtraction()
+    {
+      System.Out.Println("this is used for subtraction");   
+    }
     public static void main(String args[])
     {
         System.Out.Println("welcome to github");
