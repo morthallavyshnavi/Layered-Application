efc38c3b3c7af4a2bf6000dcb08c9709d0fdae8e
vyshu <morthallavyshnavireddy@gmail.com>
2025-01-09 21:58:48 +0530

    initial commit

--- text.java
+++ text.java
@@ -1,7 +1,13 @@
 public class Emp{
+<<<<<<< Updated upstream
     public void subtraction()
     {
       System.Out.Println("this is used for subtraction");   
+=======
+    public void add()
+    {
+        System.Out.Println("for addition");
+>>>>>>> Stashed changes
     }
     public static void main(String args[])
     {
